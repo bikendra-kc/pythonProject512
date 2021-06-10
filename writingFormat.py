efa2092c53258770faa95dacb168e4bfb8bf7549
Nitro 5 <kcbikey7@gmail.com>
2021-06-10 14:42:03 +0545

    deskRequired

--- writingFormat.py
+++ writingFormat.py
@@ -3,7 +3,7 @@ age = input ("enter the age")
 
 print("hello my name"+name+" is and i am"+age+" years old")
 print("hello my name is",name," and i am",age," years old")
-print("hello my name is %s and i am %d years old" %(name,age))
+print("hello my name is %s and i am %d years old" %(name, str(age))
 print(f"hello my name is {name} and i am {age} years old")
 print("hello my name is and i am years old")
 print("hello my name is {} and i am {} years old",format(name,age))
