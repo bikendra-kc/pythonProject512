39439c8ad33ee4b9763a9d3e124ca246799fa66c
Nitro 5 <kcbikey7@gmail.com>
2021-06-14 08:39:55 +0545

    SumOfPositiveNum

---
+++ number9.py
@@ -0,0 +1,5 @@
+#9. Write a python program to find sum of the first n positive integers.
+#sum = (n*(n+1))/2
+n=float(input("enter the positive number"))
+sum = (n*(n+1))/2
+print(f"the sum of the positive numbers is ",sum)
