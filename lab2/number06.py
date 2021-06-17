f3f233cc6eed0760a8ade93f9580057ab889f6e2
Nitro 5 <kcbikey7@gmail.com>
2021-06-17 21:08:01 +0545

    threedigitaddition

--- lab2/number06.py
+++ lab2/number06.py
@@ -1,6 +1,5 @@
 '''6. Given an integer number, print its last digit.'''
 '''
-8. Given a three-digit number. Find the sum of its digits.
 9. Check whether the given year is leap year or not. If year is leap print ‘LEAP YEAR’ else print ‘COMMON YEAR’.
 Hint: • a year is a leap year if its number is exactly divisible by 4 and is not exactly divisible by 100
 • a year is always a leap year if its number is exactly divisible by 400
---
+++ lab2/number08.py
@@ -0,0 +1,8 @@
+'''8. Given a three-digit number. Find the sum of its digits.'''
+a=str(input('enter the number:'))
+c=0
+for i in (a):
+    c=int(i)+c
+print(f'the addition of the three digit is ',c)
+
+
