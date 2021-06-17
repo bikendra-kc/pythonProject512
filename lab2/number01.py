d78c9a69dfcbebf96001ae54eabe7e5abb785e58
Nitro 5 <kcbikey7@gmail.com>
2021-06-17 14:09:02 +0545

    check

---
+++ lab2/number01.py
@@ -0,0 +1,7 @@
+'''[2:02 PM] Prerana  Pandit
+    1. Check whether 5 is in list of first 5 natural numbers or not. Hint: List => [1,2,3,4,5]
+'''
+list=(1,2,3,4,5)
+for i in list:
+    if i==5:
+        print(f'number 5 is in the list ')
