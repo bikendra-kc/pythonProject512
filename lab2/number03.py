b3b1506c5bea02f38aa0511c96b7c34c191cdddd
Nitro 5 <kcbikey7@gmail.com>
2021-06-17 14:22:16 +0545

    evenodd

---
+++ lab2/number03.py
@@ -0,0 +1,6 @@
+"""3. Check whether the user input number is even or odd and display it to user"""
+a=int(input('enter the number'))
+if a%2 ==0:
+    print(f"the number {a} is even ")
+else:
+    print(f'the number{a} is odd')
