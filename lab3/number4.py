fd2adc00202d93d13fc3f2438d938f5bab19992b
Nitro 5 <kcbikey7@gmail.com>
2021-06-24 20:45:39 +0545

    multipleandsumof3n5

---
+++ lab3/number4.py
@@ -0,0 +1,16 @@
+'''Q.No.4 Write a function that returns the sum of multiples of 3 and 5 between 0 andlimit(parameter).
+For example, if limit is 20, it should return the sum of 3, 5, 6, 9, 10, 12, 15, 18, '''
+def limited(a):
+    b=0
+    c=0
+    for i in range (a):
+        if i%3==0:
+            b=i+b
+        elif i%5==0:
+            c=i+c
+    k=c+b
+    return k
+
+k=limited(20)
+print(f'the sum of multiples of 5 and 3 between 0 and limit (parameter) is {k}')
+
