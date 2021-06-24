4f4dd95ec74af2f341671d9d290a350cc0c4f617
Nitro 5 <kcbikey7@gmail.com>
2021-06-24 21:27:55 +0545

    palindrome

---
+++ lab3/number9.py
@@ -0,0 +1,9 @@
+'''9 Write a Python function that checks whether a passed string is palindrome or not.
+Note: A palindrome is a word, phrase, or sequence that reads the same backward as forward, e.g., madam or nurses run.'''
+def palindrome(a):
+    b = str(a[len(a)::-1])
+    if a == b:
+        print(f'the string {a} is palindrome')
+    else:
+        print(f'the string {a} is not palindrome')
+palindrome('madam')
