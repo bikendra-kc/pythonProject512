ee5a18f91a9155c849f7c0d423056db0a0f23d66
Nitro 5 <kcbikey7@gmail.com>
2021-06-24 20:53:53 +0545

    print*****if5

---
+++ lab3/number5.py
@@ -0,0 +1,7 @@
+'''5 Write a function calledshow_stars(rows).If rows is
+5, it should print thefollowing:***************.'''
+def called_stars(rows):
+    if rows ==5:
+        print('?*?**??***?****?*****')
+    print('programme end')
+called_stars(5)
