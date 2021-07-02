85a29ed76d19511f1bf26c5ef0fc8cc52e89e11d
Nitro 5 <kcbikey7@gmail.com>
2021-07-02 17:28:39 +0545

    empty list

---
+++ list/number6.py
@@ -0,0 +1,7 @@
+'''6. Write a Python program to check a list is empty or not'''
+a=[]
+if len(a)==0:
+    print('the list is empty')
+else:
+    print('the list is not empty')
+print('programme end')
